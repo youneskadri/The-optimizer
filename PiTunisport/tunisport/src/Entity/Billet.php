687e039a897ca2a7998b59055bd9daa16287a579
MohamedAhmedKrichen <MohamedAhmed.Krichen@esprit.tn>
2023-02-13 20:47:28 +0100

    main

---
+++ PiTunisport/tunisport/src/Entity/Billet.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\BilletRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: BilletRepository::class)]
+class Billet
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column]
+    private ?float $prix = null;
+
+    #[ORM\OneToMany(mappedBy: 'billet', targetEntity: reservation::class)]
+    private Collection $reservation;
+
+    public function __construct()
+    {
+        $this->reservation = new ArrayCollection();
+    }
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getPrix(): ?float
+    {
+        return $this->prix;
+    }
+
+    public function setPrix(float $prix): self
+    {
+        $this->prix = $prix;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection<int, reservation>
+     */
+    public function getReservation(): Collection
+    {
+        return $this->reservation;
+    }
+
+    public function addReservation(reservation $reservation): self
+    {
+        if (!$this->reservation->contains($reservation)) {
+            $this->reservation->add($reservation);
+            $reservation->setBillet($this);
+        }
+
+        return $this;
+    }
+
+    public function removeReservation(reservation $reservation): self
+    {
+        if ($this->reservation->removeElement($reservation)) {
+            // set the owning side to null (unless already changed)
+            if ($reservation->getBillet() === $this) {
+                $reservation->setBillet(null);
+            }
+        }
+
+        return $this;
+    }
+}
---
+++ PiTunisport/tunisport/src/Entity/Hebergement.php
@@ -0,0 +1,99 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\HebergementRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: HebergementRepository::class)]
+class Hebergement
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $image = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $nomHeberg = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $deschebergement = null;
+
+    #[ORM\ManyToOne(inversedBy: 'hebergements')]
+    private ?localisation $localisation = null;
+
+    #[ORM\ManyToOne(inversedBy: 'hebergements')]
+    private ?categoryhebergement $categoryHebergement = null;
+
+ 
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getImage(): ?string
+    {
+        return $this->image;
+    }
+
+    public function setImage(string $image): self
+    {
+        $this->image = $image;
+
+        return $this;
+    }
+
+    public function getNomHeberg(): ?string
+    {
+        return $this->nomHeberg;
+    }
+
+    public function setNomHeberg(string $nomHeberg): self
+    {
+        $this->nomHeberg = $nomHeberg;
+
+        return $this;
+    }
+
+    public function getDescHebergement(): ?string
+    {
+        return $this->deschebergement;
+    }
+
+    public function setDescHebergement(string $deschebergement): self
+    {
+        $this->deschebergement = $deschebergement;
+
+        return $this;
+    }
+
+    public function getLocalisation(): ?localisation
+    {
+        return $this->localisation;
+    }
+
+    public function setLocalisation(?localisation $localisation): self
+    {
+        $this->localisation = $localisation;
+
+        return $this;
+    }
+
+    public function getCategoryHebergement(): ?categoryhebergement
+    {
+        return $this->categoryHebergement;
+    }
+
+    public function setCategoryHebergement(?categoryhebergement $categoryHebergement): self
+    {
+        $this->categoryHebergement = $categoryHebergement;
+
+        return $this;
+    }
+
+   
+}
